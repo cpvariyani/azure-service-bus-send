240e911325ec931cc280dcb3ab9847ec0be9dbd6
V-Chandra Variyani <Chandra.Variyani@blackbaud.me>
2019-04-03 12:05:03 +0530

    data

---
+++ user-send-microservice/user-send-microservice/Program.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.Azure.ServiceBus;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace user_send_microservice
+{
+    class Program
+    {
+        static ITopicClient topicClient;
+        static void Main(string[] args)
+        {
+            MainAsync().GetAwaiter().GetResult();
+        }
+
+        static async Task MainAsync()
+        {
+            string ServiceBusConnectionString = "yourtopic-subscription-send-key-goes-here";
+            string TopicName = "your-topic-name";
+
+            topicClient = new TopicClient(ServiceBusConnectionString, TopicName);
+
+            // Send messages.
+            await SendUserMessage();
+
+            Console.ReadKey();
+
+            await topicClient.CloseAsync();
+        }
+
+
+        static async Task SendUserMessage()
+        {
+            List<User> users = GetDummyDataForUser();
+
+            var serializeUser = JsonConvert.SerializeObject(users);
+
+            string messageType = "userData";
+
+            string messageId = Guid.NewGuid().ToString();
+
+            var message = new ServiceBusMessage
+            {
+                Id = messageId,
+                Type = messageType,
+                Content = serializeUser
+            };
+
+            var serializeBody = JsonConvert.SerializeObject(message);
+
+            // send data to bus
+
+            var busMessage = new Message(Encoding.UTF8.GetBytes(serializeBody));
+            busMessage.UserProperties.Add("Type", messageType);
+            busMessage.MessageId = messageId;
+
+            await topicClient.SendAsync(busMessage);
+
+            Console.WriteLine("message has been sent");
+
+        }
+
+       public class User
+        {
+            public int Id { get; set; }
+
+            public string Name { get; set; }
+        }
+
+        public class ServiceBusMessage
+        {
+            public string Id { get; set; }
+            public string Type { get; set; }
+            public string Content { get; set; }
+        }
+
+        private static List<User> GetDummyDataForUser()
+        {
+            User user = new User();
+            List<User> lstUsers = new List<User>();
+            for (int i = 1; i < 3; i++)
+            {
+                user = new User();
+                user.Id = i;
+                user.Name = "CPVariyani" + i;
+
+                lstUsers.Add(user);
+            }
+
+            return lstUsers;
+        }
+    }
+}
